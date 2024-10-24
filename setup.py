cd37cc3df8330cef2f528b212a99fe4fc39c4fd0
prasadsdd <prasaddhumal083@gmail.com>
2024-10-24 14:27:23 +0530

    "New code added"

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Chatbot',
+    version= '0.0.0',
+    author= 'Prasad',
+    author_email= 'pashdhumal083@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
---
+++ src/helper.py
@@ -0,0 +1,31 @@
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+#from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.embeddings import BedrockEmbeddings
+
+
+#Extract Data From the PDF File
+def load_pdf_file(data):
+    loader= DirectoryLoader(data,
+                            glob="*.pdf",
+                            loader_cls=PyPDFLoader)
+
+    documents=loader.load()
+
+    return documents
+
+
+
+#Split the Data into Text Chunks
+def text_split(extracted_data):
+    text_splitter=RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
+    text_chunks=text_splitter.split_documents(extracted_data)
+    return text_chunks
+
+
+
+
+#Download the Embeddings from HuggingFace 
+def download_embeddings():
+    embeddings=BedrockEmbeddings(model_id="amazon.titan-embed-text-v1", client=bedrock)  #this model return 384 dimensions
+    return embeddings
